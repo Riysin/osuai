82d5f338e729645aca195e3e27f3af2c9578a3f4
Riysin <rickylin1007@gmail.com>
2024-01-10 16:31:02 +0800

    optimized+auto reconnection, successfully gets websocketdata

--- project/get_api.py
+++ project/get_api.py
@@ -1,24 +1,16 @@
 import websockets
 import json
-import asyncio
 
 async def connect_websocket(uri):
-    try:
-        return await websockets.connect(uri)
-    except websockets.exceptions.ConnectionClosedError:
-        return await asyncio.sleep(5)
-    except Exception as e:
-        print(f"Error: {e}")
+    return await websockets.connect(uri, timeout = 0.1)
 
 async def get_data(websocket):
     data = await websocket.recv()
     jdata = json.loads(data)
     return jdata
 
-async def game_state(websocket):
-    data = await get_data(websocket)
+def game_state(data):
     return data['menu']['state']
 
-async def hit_score(websocket, score):
-    data = await get_data(websocket)
+def hit_score(data, score):
     return data['gameplay']['hits'][str(score)]
--- project/main.py
+++ project/main.py
@@ -1,26 +1,28 @@
 import asyncio
 import time
 import websockets
-from get_api import connect_websocket, game_state, hit_score
+from get_api import *
 
 async def main():
     uri = "ws://localhost:24050/ws"
     websocket = await connect_websocket(uri)
     while True:
         try:
-            if await game_state(websocket) == 2:
-                hit300 = await hit_score(websocket, 300)
-                hit100 = await hit_score(websocket, 100)
-                hit50 = await hit_score(websocket, 50)
-                hit0 = await hit_score(websocket, 0)
+            data = await get_data(websocket)
+            if game_state(data) == 2:
+                hit300 = hit_score(data, 300)
+                hit100 = hit_score(data, 100)
+                hit50 = hit_score(data, 50)
+                hit0 = hit_score(data, 0)
                 print('wowwi')
                 print(f"300:{hit300}, 100:{hit100}, 50:{hit50}, miss:{hit0}")
             else:
                 print('test')
             time.sleep(0.1)
         except websockets.exceptions.ConnectionClosedError:
-            print('ctimeout')
-            await time.sleep(0.1)
+            print('Reconnecting in 1...')
+            time.sleep(1)
+            websocket = await connect_websocket(uri)       
 
 if __name__ == "__main__":
     asyncio.run(main())
